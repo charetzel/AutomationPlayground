c6b897e600435e2ceaf3987ea952c2893e8455c8
charetzel <161915151+charetzel@users.noreply.github.com>
2024-03-08 15:27:59 +0800

    Update

--- src/base/base_locators.py
+++ src/base/base_locators.py
@@ -6,15 +6,6 @@ class BasePageLocators:
     def format_locator(
         locator: Tuple[str, str], *args, **kwargs
     ) -> Tuple[str, str]:
-        """
-        Format **selector** part of the locator.
-
-            Example:
-        `format((By.XPATH, '//td[parent::tr/td/text()="{}"]'), 'Chrome')`
-
-            Output:
-        `(By.XPATH, '//td[parent::tr/td/text()="Chrome"]')`
-        """
         by, value = locator
         formatted_value = value.format(*args, **kwargs)
         return by, formatted_value
